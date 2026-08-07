fe04897864f006420600ce52af9fd7d4f66a9d76
fra-dev15 <frafra.dev@gmail.com>
2026-08-07 15:12:04 +0900

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,5 +1,6 @@
 const dataTableBody = document.getElementById('dataTableBody');
-const API_BASE_URL = 'https://prompt-san.vercel.app';
+// const API_BASE_URL = 'https://prompt-san.vercel.app';
+const API_BASE_URL = 'https://pick-style.vercel.app';
 
 // データを取得してテーブルを作る関数
 async function fetchAndRenderData() {
@@ -8,7 +9,7 @@ async function fetchAndRenderData() {
     dataTableBody.innerHTML = '<tr><td colspan="2" style="text-align:center;">読み込み中...</td></tr>';
 
     // APIを叩く
-    const API_BASE_URL = 'https://pick-style.vercel.app';
+    const response = await fetch(`${API_BASE_URL}/api/get-all`);
     
     if (!response.ok) throw new Error('API通信エラー');
     
